33f1bde35fbed89f69374a3c1eee74aa3bdc7e47
Sulav Neupane <sulav@yombu.com>
2019-05-03 12:41:09 -0400

    Using external dependency in gradle

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+€
+Ptoasterlibrary/src/main/java/com/nepalicoders/toasterlibrary/ToasterMessage.java,a/9/a9177466266d195b7436fb6a61cb81eb855835f6
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
--- toasterlibrary/src/main/java/com/nepalicoders/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/nepalicoders/toasterlibrary/ToasterMessage.java
@@ -3,10 +3,26 @@ package com.nepalicoders.toasterlibrary;
 import android.content.Context;
 import android.widget.Toast;
 
+import timber.log.Timber;
+
 public class ToasterMessage {
 
-    public static void show(Context c, String message) {
+    private static ToasterMessage toasterMessage;
+
+    private ToasterMessage() {
+        Timber.plant(new Timber.DebugTree());
+    }
+
+    public static ToasterMessage getInstance() {
+        if (toasterMessage == null) {
+            toasterMessage = new ToasterMessage();
+        }
+        return toasterMessage;
+    }
+
+    public void show(Context c, String message) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+        Timber.d(message);
     }
 
 }
